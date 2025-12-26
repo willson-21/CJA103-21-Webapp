061d4b8adf0eeb254bc9975735dfcbb65b73c81b
Willson <goal151234560@gmail.com>
2025-12-26 10:51:53 +0800

    push test

---
+++ src/main/java/com/area/AreaVO.java
@@ -0,0 +1,32 @@
+package com.area;
+
+public class AreaVO {
+
+	private Integer areaid;
+	private String cityname;
+	private String district;
+
+	public Integer getAreaid() {
+		return areaid;
+	}
+
+	public void setAreaid(Integer areaid) {
+		this.areaid = areaid;
+	}
+
+	public String getCityname() {
+		return cityname;
+	}
+
+	public void setCityname(String cityname) {
+		this.cityname = cityname;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+}
